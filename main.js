2808449c7bd986eb1a9f0cd7612d40b61b1834a6
Kamil Figiela <kamil.figiela@gmail.com>
2014-05-16 17:44:33 +0200

    Improvements

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var deg2rad = function(deg) { return deg * (Math.PI/180); }
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+
+
+
+
 var android = navigator.userAgent.match(/android/ig);
 
 $(document).ready(function(){
@@ -6,17 +25,26 @@ $(document).ready(function(){
     var sortPlaces = function(places) {
         if(geolocation) {
             return _.sortBy(places, function(place) { 
-                // This should be good-enough approximation
-                var x = geolocation.coords.latitude-parseFloat($(place).attr('lat'));
-                var y = geolocation.coords.longitude-parseFloat($(place).attr('lng'));
-                return Math.sqrt(x*x+y*y);
+                return parseFloat($(place).attr('distance'));
             });
         } else {
             return _.sortBy(places, function(place) { return $(place).attr('name'); });
         }
     }
     
+    var updateDistances = function() {
+        if(places && geolocation) {
+            places.forEach(function(_place){
+                var place = $(_place);
+                var distance = getDistanceFromLatLonInKm(geolocation.coords.latitude, geolocation.coords.longitude, parseFloat(place.attr('lat')), parseFloat(place.attr('lng')));
+                place.attr('distance', distance);
+            });
+        }
+    };
+    
     var display = function(){
+        var template = _.template("<tr><td><a href=\"<%= prefix %><%= q %>\"><%= name %></a> <small><%= distance  %></small></td><td><span class=\"label pull-right <%= status %>\"><%= bikes %></tr>");
+        
         $("#rowery > tbody").html('');
         places = sortPlaces(places);
         var list = places.forEach(function(_place) {
@@ -29,7 +57,9 @@ $(document).ready(function(){
             var params = {
                     name: place.attr('name'), 
                     bikes: place.attr('bikes'), 
+                    distance: (place.attr('distance')?'('+(parseFloat(place.attr('distance'))).toFixed(1)+' km)':''), 
                     status: status,
+                    prefix: android?'geo:':'https://maps.google.com/maps?q=',
                     q: [place.attr('lat'),place.attr('lng')].join(",")
                 };
             
@@ -38,21 +68,19 @@ $(document).ready(function(){
     };
     
     var geoloc = function(_geolocation) {
+        console.log(["Got location",_geolocation]);
         geolocation = _geolocation;
+        updateDistances();
         if(places)
             display();
     };
+
+    if(navigator.geolocation) navigator.geolocation.getCurrentPosition(geoloc);
     
-    navigator.geolocation.getCurrentPosition(geoloc);
-    
-    var template;
-    if(android)
-        template = _.template("<tr><th><a href=\"geo:<%= q %>\"><%= name %></a></th><td><span class=\"label pull-right <%= status %>\"><%= bikes %></tr>");
-    else
-     template = _.template("<tr><th><a href=\"https://maps.google.com/maps?q=<%= q %>\"><%= name %></a></th><td><span class=\"label pull-right <%= status %>\"><%= bikes %></tr>");
     $.get('https://nextbike.net/maps/nextbike-official.xml?&domains=kp&maponly=1').done(function(data){
         $("#info").html('');    
-        places = $(data).find('place')
+        places = $(data).find('place');
+        updateDistances();
         display();
     }).fail(function(err){
         $("#info").html('<div class="alert alert-danger">Coś poszło nie tak :(</div>');
